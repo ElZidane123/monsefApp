9ce3d443859f0d27139f2146c11a8bb4018af6a3
Rameyy00 <zolaziyan2616@gmail.com>
2026-04-29 14:47:17 +0700

    avis

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const Scaffold(
         body: Center(
-          child: Text('Hello, World!'),
+          child: Text('Hello, mpek'),
         ),
       ),
     );
